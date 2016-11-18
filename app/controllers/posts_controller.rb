bff309ec1e33b03f95f94f88fd3f6f42b08be9e8
Ben Mangelsen <bmangelsen@gmail.com>
2016-11-18 15:04:37 -0500

    Added ability to create, edit, and delete a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,41 @@ class PostsController < ApplicationController
     @posts = Post.where("user_id = ?", current_user.id)
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:name, :link, :user_id)
+  end
+
   # @animals = Animal.where("name LIKE ? OR species LIKE ?", "%#{params['q']}%", "%#{params['q']}%")
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "can get posts index view" do
+    post session_path, params: { username: users(:ben).username, password: "test" }
     get posts_path
     assert_match(/Here are all the posts!/, response.body)
   end
@@ -10,5 +11,34 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     post session_path, params: { username: users(:ben).username, password: "test" }
     get user_posts_path(users(:ben).id)
     assert_match(/Here are all your posts!/, response.body)
+  end
+
+  test "can get new post view" do
+    post session_path, params: { username: users(:ben).username, password: "test" }
+    get new_post_path
+    assert_match(/Enter the info for your new post!/, response.body)
+  end
+
+  test "can create new post" do
+    post posts_path, params: { post: { name: "OMG cat pic", link: "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg", user_id: users(:ben).id }}
+    assert_equal "OMG cat pic", Post.last.name
+  end
+
+  test "can get post edit view" do
+    post session_path, params: { username: users(:ben).username, password: "test" }
+    get edit_post_path(posts(:bens_post).id)
+    assert_match(/Edit the details of your post!/, response.body)
+  end
+
+  test "can edit post" do
+    post session_path, params: { username: users(:ben).username, password: "test" }
+    patch post_path(posts(:bens_post).id), params: { post: { name: "new post name" }}
+    assert_equal "new post name", Post.find_by(id: posts(:bens_post).id).name
+  end
+
+  test "can delete post" do
+    post session_path, params: { username: users(:ben).username, password: "test" }
+    delete post_path(posts(:bens_post).id)
+    assert 2, Post.count
   end
 end
