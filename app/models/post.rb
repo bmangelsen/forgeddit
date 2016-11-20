619928652171528add8f3584e73bcb722c5c7419
Ben Mangelsen <bmangelsen@gmail.com>
2016-11-20 09:32:12 -0500

    Increased size of post name validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 60 }
   validates :link, presence: true
   validates :link, format: { with: /\A(http|https):\/\//, :message => "must include http:// or https://" }
 end
