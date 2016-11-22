eb3a0d1c01ae39697f7725867d36416192c18ad4
Ben Mangelsen <bmangelsen@gmail.com>
2016-11-22 11:24:21 -0500

    Created vote relation to post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :votes
   validates :name, presence: true, length: { maximum: 60 }
   validates :link, presence: true
   validates :link, format: { with: /\A(http|https):\/\//, :message => "must include http:// or https://" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_reader :password
   has_many :comments
   has_many :posts
+  has_many :votes
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true, :on => :create
